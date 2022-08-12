57121293d3031836d45b7d38eedf3d8e4fe4015e
prachi-kedar <prachikedar8@gmail.com>
2022-08-12 10:41:03 +0530

    configuration file

---
+++ config.py
@@ -0,0 +1,12 @@
+# path of the baground images
+BAGROUND_IMG_DIRECTORY = '/home/neosoft/Desktop/projects/automatic_object_annotation/bag'
+
+# path of an object images
+OBJECT_IMG_DIRECTORY = '/home/neosoft/Desktop/projects/automatic_object_annotation/bottle/'
+
+# path of an output folder
+OUTPUT_DIRECTORY = '/home/neosoft/Desktop/projects/automatic_object_annotation/out'
+
+# no of objects per image
+NO_OF_OBJECTS = range(2)
+
